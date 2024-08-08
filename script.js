42ba4d9bb08d3af50c05ee60e0490c5b3d564218
Peter Sharp <peter@petersharp.org>
2024-08-08 14:30:12 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function getLocation() {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        document.getElementById("grid-reference").innerHTML = "Geolocation is not supported by this browser.";
+    }
+    console.log("location found")
+}
+
+function showPosition(position) {
+    const latLng = { lat: position.coords.latitude, lng: position.coords.longitude};
+    const osGridRef = convertToOSGridRef(latLng);
+    document.getElementById("grid-reference").innerHTML = osGridRef;
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            document.getElementById("grid-reference").innerHTML = "User denied the request for Geolocation.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            document.getElementById("grid-reference").innerHTML = "Location information is unavailable.";
+            break;
+        case error.TIMEOUT:
+            document.getElementById("grid-reference").innerHTML = "The request to get user location timed out.";
+            break;
+        case error.UNKNOWN_ERROR:
+            document.getElementById("grid-reference").innerHTML = "An unknown error occurred.";
+            break;
+    }
+}
+
+function convertToOSGridRef(latLng) {
+    proj4.defs('EPSG:27700', '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs');
+    const eaNo = os.Transform.fromLatLng(latLng);
+    const gridRef = os.Transform.toGridRef(eaNo);
+    return gridRef.text;
+}
