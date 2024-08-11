95abb83373776e0f3487af53dea79a5abf898431
Peter Sharp <peter@petersharp.org>
2024-08-11 09:28:28 +0100

    Updated info message

--- script.js
+++ script.js
@@ -1,108 +1,125 @@
 function getLocation() {
-    infoMessage("Finding location...")
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-        document.getElementById("waiting").innerHTML = "Geolocation is not supported by this browser.";
-    }
+  infoMessage("Finding location...");
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    document.getElementById("info-div").innerHTML =
+      "Geolocation is not supported by this browser.";
+  }
 }
 
 function showPosition(position) {
-    const latLng = { lat: position.coords.latitude, lng: position.coords.longitude};
-    const accuracy = position.coords.accuracy;
-    const accuracyStr = accuracy.toLocaleString(undefined, {maximumFractionDigits: 0});
-    if (accuracy > 250) {
-        showErrorButton("Accuracy is low.");
-    } else {
-        hideErrorButton();
-    }
-    const osGridRef = convertToOSGridRef(latLng);
-    var osLink;
-    if (osGridRef) {
-        infoHTML = `<p>Your grid reference is</p><p class="gridref">${osGridRef}</p><p>Based on an accuracy of ${accuracyStr}m`;
-        osLink = `https://explore.osmaps.com/pin?lat=${latLng.lat}&lon=${latLng.lng}&zoom=16`;
-    } else {
-        infoHTML = 'Error finding your grid reference. You may have success with <a href="https://locate.what3words.com">What3Words Locate</a> instead';
-    }
+  const latLng = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude,
+  };
+  const accuracy = position.coords.accuracy;
+  const accuracyStr = accuracy.toLocaleString(undefined, {
+    maximumFractionDigits: 0,
+  });
+  if (accuracy > 250) {
+    showErrorButton("Accuracy is low.");
+  } else {
+    hideErrorButton();
+  }
+  const osGridRef = convertToOSGridRef(latLng);
+  var osLink;
+  if (osGridRef) {
+    console.log("test");
+    infoHTML = `<p>Your grid reference is</p><p class="gridref">${osGridRef}</p><p>Based on an accuracy of ${accuracyStr}m`;
     infoMessage(infoHTML);
-    document.getElementById("result-links").style.display = 'block';
-    document.getElementById("osmaps-link").setAttribute('href', osLink);
+    osLink = `https://explore.osmaps.com/pin?lat=${latLng.lat}&lon=${latLng.lng}&zoom=16`;
+    document.getElementById("result-links").style.display = "block";
+    document.getElementById("osmaps-link").setAttribute("href", osLink);
     const params = new URLSearchParams(window.location.search);
     if (params.has("testing")) {
-        document.getElementById("osmaps-link").style.display = 'block';
+      document.getElementById("osmaps-link").style.display = "block";
     }
-    var shareLink = document.getElementById("share-link")
-    shareLink.addEventListener("click", () => shareLocation(osGridRef, accuracyStr));
+    var shareLink = document.getElementById("share-link");
+    shareLink.addEventListener("click", () =>
+      shareLocation(osGridRef, accuracyStr)
+    );
+  }
 }
 
 function convertToOSGridRef(latLng) {
-    proj4.defs('EPSG:27700', '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs');
-    var inBounds=os.Transform._checkBounds(latLng);
-    if (inBounds.valid) {
-        const eaNo = os.Transform.fromLatLng(latLng);
-        const gridRef = os.Transform.toGridRef(eaNo);
-        // return gridRef.text;
-        return `${gridRef.letters} ${gridRef.eastings.substring(0,3)}${gridRef.northings.substring(0,3)}`;
-    } else {
-        showErrorButton(inBounds.message);
-        return undefined;
-    }
-
+  proj4.defs(
+    "EPSG:27700",
+    "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs"
+  );
+  var inBounds = os.Transform._checkBounds(latLng);
+  if (inBounds.valid) {
+    const eaNo = os.Transform.fromLatLng(latLng);
+    const gridRef = os.Transform.toGridRef(eaNo);
+    // return gridRef.text;
+    return `${gridRef.letters} ${gridRef.eastings.substring(
+      0,
+      3
+    )}${gridRef.northings.substring(0, 3)}`;
+  } else {
+    showErrorButton(inBounds.message);
+    return undefined;
+  }
 }
 
 function shareLocation(osGridRef, accuracyStr) {
-    d = new Date();
-    const shareText = `At ${d.toLocaleTimeString()} on ${d.toLocaleDateString()} my location was ${osGridRef}, with an accuracy of ${accuracyStr}m. View location online: ${document.getElementById("osmaps-link").getAttribute("href")}`;
-    if(navigator.share) {
-        try {
-            navigator.share({text: shareText});
-        } catch (error) {
-            alternateShare(shareText);
-        }
-    } else {
-        alternateShare(shareText);
+  d = new Date();
+  const shareText = `At ${d.toLocaleTimeString()} on ${d.toLocaleDateString()} my location was ${osGridRef}, with an accuracy of ${accuracyStr}m. View location online: ${document
+    .getElementById("osmaps-link")
+    .getAttribute("href")}`;
+  if (navigator.share) {
+    try {
+      navigator.share({ text: shareText });
+    } catch (error) {
+      alternateShare(shareText);
     }
+  } else {
+    alternateShare(shareText);
+  }
 }
 
 function alternateShare(shareText) {
-    const copyButtonLabel = "Copy message";
-    altShareDiv = document.getElementById("alt-share-div");
-    altShareDiv.style.display = 'block';
-    const heading = document.createElement('h4');
-    heading.textContent = 'Error sharing, copy text below';
-    const p = document.createElement('p');
-    p.textContent = shareText;
-    altShareDiv.appendChild(heading);
-    altShareDiv.appendChild(p);
+  const copyButtonLabel = "Copy message";
+  altShareDiv = document.getElementById("alt-share-div");
+  altShareDiv.style.display = "block";
+  const heading = document.createElement("h4");
+  heading.textContent = "Error sharing, copy text below";
+  const p = document.createElement("p");
+  p.textContent = shareText;
+  altShareDiv.appendChild(heading);
+  altShareDiv.appendChild(p);
 }
 
 function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            showErrorButton("User denied the request for Geolocation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            showErrorButton("Location information is unavailable.");
-            break;
-        case error.TIMEOUT:
-            showErrorButton("The request to get user location timed out.");
-            break;
-        case error.UNKNOWN_ERROR:
-            showErrorButton("An unknown error occurred.");
-            break;
-    }
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      showErrorButton("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      showErrorButton("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      showErrorButton("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      showErrorButton("An unknown error occurred.");
+      break;
+  }
 }
 function showErrorButton(errorText) {
-    errorButton = document.getElementById('error-btn');
-    errorButton.style.display = 'block';
-    errorButton.innerHTML = `${errorText} Tap to refresh`
-    errorButton.addEventListener("click", getLocation)
+  errorButton = document.getElementById("error-btn");
+  errorButton.style.display = "block";
+  errorButton.innerHTML = `${errorText} Tap to refresh`;
+  infoHTML =
+  'Error finding your grid reference. You may have success with <a href="https://locate.what3words.com">What3Words Locate</a> instead';
+  infoMessage(infoHTML);
+  errorButton.addEventListener("click", getLocation);
 }
 
 function hideErrorButton() {
-    document.getElementById('error-btn').style.display = 'None';
+  document.getElementById("error-btn").style.display = "None";
 }
 
 function infoMessage(HTML) {
-    document.getElementById("info-message").innerHTML = HTML
+  document.getElementById("info-message").innerHTML = HTML;
 }
